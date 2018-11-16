f04c3f7e65971c756e5444306da0597549bc67bf
Harrison Banh <harrisonbanh+gitlab@gmail.com>
2018-11-16 08:39:11 -0500

    [Teacher Home]
    Fixed vertical constraints.

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/TeacherHome.java
@@ -0,0 +1,14 @@
+package com.example.jda8301.spellarhyme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TeacherHome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_home);
+    }
+
+}
