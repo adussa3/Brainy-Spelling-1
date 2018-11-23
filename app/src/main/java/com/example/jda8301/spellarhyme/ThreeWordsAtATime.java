566fd332a9f027af6d80440547782c2cbdbc01eb
Anunoy Dussa <adussa3@gmail.com>
2018-11-23 17:03:58 -0500

    Merge branch 'master' into user_files

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/ThreeWordsAtATime.java
@@ -0,0 +1,13 @@
+package com.example.jda8301.spellarhyme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThreeWordsAtATime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_words_at_atime);
+    }
+}
