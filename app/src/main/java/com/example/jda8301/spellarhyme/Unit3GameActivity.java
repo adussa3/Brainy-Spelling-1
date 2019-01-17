14a9b33da22e90ebe3861ea3bbe79624836e737c
Anunoy Dussa <adussa3@gmail.com>
2019-01-16 23:13:57 -0500

    Created activities for Unit 3
    Create basic activities for Unit 3:
    (1) Unit3VowelList
    (2) Unit3WordList
    (3) Unit3Game

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/Unit3GameActivity.java
@@ -0,0 +1,13 @@
+package com.example.jda8301.spellarhyme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Unit3GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit3_game);
+    }
+}
