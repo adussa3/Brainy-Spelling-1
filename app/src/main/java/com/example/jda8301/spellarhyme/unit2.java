22095fbd787ba2a4683134984231da9b3c24831a
Tiffany Xu <txtxxu@gmail.com>
2019-01-16 20:07:17 -0500

    initial commit

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/unit2.java
@@ -0,0 +1,14 @@
+package com.example.jda8301.spellarhyme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class unit2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit2);
+    }
+
+}
