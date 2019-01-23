9e0b8664055dfc607f5d7d0ab4c0ac798c719e63
Yizra <yizra.g@gmail.com>
2019-01-23 17:49:57 -0500

    created word classes

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/model/SegmentedWord.java
@@ -0,0 +1,12 @@
+package com.example.jda8301.spellarhyme.model;
+
+/**
+ * Created by YizraGhebre on 1/23/19.
+ */
+
+public class SegmentedWord {
+    private String displayString;
+    private boolean landscape;
+    private int[] soundFiles;
+    private int[] audioFiles[];
+}
---
+++ app/src/main/java/com/example/jda8301/spellarhyme/model/VowelWord.java
@@ -0,0 +1,13 @@
+package com.example.jda8301.spellarhyme.model;
+
+/**
+ * Created by YizraGhebre on 1/23/19.
+ */
+
+public class VowelWord {
+    private String stringName;
+    private int[] soundID;
+    private int[] silentLetters;
+    private int[] vowels[];
+    private int[] targetVowel[];
+}
