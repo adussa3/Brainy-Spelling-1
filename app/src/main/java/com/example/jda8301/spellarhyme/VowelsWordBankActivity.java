e9684e8dc94dd77023ca1e0c68b91fb811ab8606
Tiffany Xu <txtxxu@gmail.com>
2019-02-08 11:54:50 -0500

    progress in filtering images instead of using BW versions

--- app/src/main/java/com/example/jda8301/spellarhyme/VowelsWordBankActivity.java
+++ app/src/main/java/com/example/jda8301/spellarhyme/VowelsWordBankActivity.java
@@ -1,12 +1,21 @@
 package com.example.jda8301.spellarhyme;
 
 import android.content.Intent;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.jda8301.spellarhyme.utils.Bank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class VowelsWordBankActivity extends AppCompatActivity {
     @Override
@@ -19,9 +28,10 @@ public class VowelsWordBankActivity extends AppCompatActivity {
         TextView actionBarTitle = actionBar.findViewById(R.id.actionBarTitle);
         actionBarTitle.setText("Vowels Word Bank");
 
-        //update pictures to display which ones are learned
+        //update pictures to display which ones are learned--colored
         //TODO: replace with actual user name that isn't hard coded
-        Bank.updateLearnedWords("default", "vowels", this);
+        updateImages();
+
 
     }
 
@@ -29,6 +39,40 @@ public class VowelsWordBankActivity extends AppCompatActivity {
     public void onClickExit(View view) {
         Intent intent = new Intent(getApplicationContext(), BankActivity.class);
         startActivity(intent);
+    }
+
+    private static void findImageViews(ViewGroup viewGroup,ArrayList<ImageButton> views) {
+        for (int i = 0, N = viewGroup.getChildCount(); i < N; i++) {
+            View child = viewGroup.getChildAt(i);
+            if (child instanceof ViewGroup) {
+                findImageViews((ViewGroup) child, views);
+            } else if (child instanceof ImageButton) {
+                views.add((ImageButton) child);
+            }
+        }
+    }
+
+    public ArrayList<ImageButton> getImageViews() {
+        ArrayList<ImageButton> buttonViews = new ArrayList<>();
+        ViewGroup viewGroup = (ViewGroup) getWindow().getDecorView();
+        findImageViews(viewGroup, buttonViews);
+        return buttonViews;
+    }
+
+    public void updateImages() {
+
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.setSaturation(0);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+
+        List<ImageButton> imageButtons = getImageViews();
+        for (ImageButton button : imageButtons) {
+            String word = (String) button.getContentDescription();
+            if (Bank.getSpellCount("default", "vowels", word) < 3) {
+                button.getDrawable().setColorFilter(filter);
+            }
+        }
     }
 
 }
