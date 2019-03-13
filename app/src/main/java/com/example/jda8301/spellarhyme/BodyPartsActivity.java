0240c54e05ed299771451a788e94195f325b5a93
Harrison Banh <harrisonbanh+gitlab@gmail.com>
2019-03-13 10:31:05 -0400

    [Body Parts Bank]
    Added in body parts bank files

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/BodyPartsActivity.java
@@ -0,0 +1,14 @@
+package com.example.jda8301.spellarhyme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BodyPartsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_parts);
+    }
+
+}
