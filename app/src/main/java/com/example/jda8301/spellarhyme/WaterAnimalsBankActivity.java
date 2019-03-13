1d3f04dd2af2f0c557e6dad9eaac62037733fe7c
Harrison Banh <harrisonbanh+gitlab@gmail.com>
2019-03-13 10:42:02 -0400

    [Water Animals Bank]
    Added in Water Animals bank files

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/WaterAnimalsBankActivity.java
@@ -0,0 +1,14 @@
+package com.example.jda8301.spellarhyme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class WaterAnimalsBankActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_animals_bank);
+    }
+
+}
