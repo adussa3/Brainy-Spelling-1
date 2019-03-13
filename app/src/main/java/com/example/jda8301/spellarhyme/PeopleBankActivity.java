dd0eb3f39475a19458f8858b3de51d2bb654e3e3
Tiffany Xu <txtxxu@gmail.com>
2019-03-13 10:26:33 -0400

    people bank activity and xml added

---
+++ app/src/main/java/com/example/jda8301/spellarhyme/PeopleBankActivity.java
@@ -0,0 +1,14 @@
+package com.example.jda8301.spellarhyme;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PeopleBankActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people_bank);
+    }
+
+}
