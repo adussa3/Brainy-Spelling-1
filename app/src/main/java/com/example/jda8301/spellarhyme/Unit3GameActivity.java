b02a1ac8b39d65f671bfcdf0e6e24b44baffb86d
bolbilabs <bolbilabs@users.noreply.github.com>
2019-03-13 10:25:38 -0400

    [Unit 3] Created new branch

--- app/src/main/java/com/example/jda8301/spellarhyme/Unit3GameActivity.java
+++ app/src/main/java/com/example/jda8301/spellarhyme/Unit3GameActivity.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.example.jda8301.spellarhyme.data.AppPreferencesHelper;
+import com.example.jda8301.spellarhyme.model.SegmentedWord;
+
+import java.util.List;
 
 public class Unit3GameActivity extends AppCompatActivity {
 
     private ImageView exit;
 
+    private Button[] buttons;
+
+    private LinearLayout fieldLayout;
+
+    // Initializes AppPreferencesHelper to read JSON files
+    AppPreferencesHelper helper = new AppPreferencesHelper();
+//    List<List<SegmentedWord>> segmentedWords = helper.getVowels();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit3_game);
+
+        initializeComponents();
 
         View actionBar = findViewById(R.id.actionBar);
         TextView actionBarTitle = actionBar.findViewById(R.id.actionBarTitle);
@@ -32,4 +49,22 @@ public class Unit3GameActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), Unit3WordListActivity.class);
         startActivity(intent);
     }
+
+
+    public void initializeComponents() {
+        buttons[0] = (Button) findViewById(R.id.button0);
+        buttons[1] = (Button) findViewById(R.id.button1);
+        buttons[2] = (Button) findViewById(R.id.button2);
+        buttons[3] = (Button) findViewById(R.id.button3);
+        buttons[4] = (Button) findViewById(R.id.button4);
+        buttons[5] = (Button) findViewById(R.id.button5);
+
+
+        fieldLayout = (LinearLayout) findViewByID(R.id.fieldLayout);
+
+
+
+    }
+
+
 }
